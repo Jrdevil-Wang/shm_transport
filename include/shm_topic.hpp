7517162067c43a9e986cde4382ace270907cf00d
Hujun <hujun1413@foxmail.com>
2017-08-09 13:46:30 +0800

    BUGFIX TODO 1. Subscribe to the same topic in one procedure will be wrong 2. When a subscriber exit, there may be one message left in the shared memory space.
    1. Due to the implemention of normal ROS subscriber, it will be something
    wrong when subscribe to the same topic in one procedure, for the
    publisher will think there is only one subscriber listen to the topic,
    but actually there are two. Maybe we can open an issue on github ros_comm.
    
    2. When a subscriber exit, the publisher may not know the
    decreace of the number of subscribers, so it still publish
    more message than exactly what is. Then the extra message
    will be left in the shared memory space. If the space is not
    that big or subscribers exit many times, it will throw bad_alloc exception.

--- include/shm_topic.hpp
+++ include/shm_topic.hpp
@@ -12,32 +12,29 @@
 namespace shm_transport {
 
   class Topic {
-    public:
-      Topic(const ros::NodeHandle & parent) {
-        nh_ = boost::make_shared<ros::NodeHandle>(parent);
-      }
+  public:
+    Topic(const ros::NodeHandle & parent) {
+      nh_ = boost::make_shared<ros::NodeHandle>(parent);
+    }
 
-      ~Topic() {
-      }
+    ~Topic() {
+    }
 
-      template < class M >
-      Publisher advertise(const std::string & topic, uint32_t queue_size, uint32_t mem_size) {
-        ros::Publisher pub = nh_->advertise< std_msgs::UInt64 >(topic, queue_size);
-        boost::interprocess::managed_shared_memory * pshm = new boost::interprocess::managed_shared_memory(boost::interprocess::open_or_create, topic.c_str(), mem_size);
-        boost::atomic<uint32_t> *ref_ptr = pshm->find_or_construct<boost::atomic<uint32_t> >("ref")(0);
-        ref_ptr->fetch_add(1, boost::memory_order_relaxed);
-        return Publisher(pub, pshm);
-      }
+    template < class M >
+    Publisher advertise(const std::string & topic, uint32_t queue_size, uint32_t mem_size) {
+      ros::Publisher pub = nh_->advertise< std_msgs::UInt64 >(topic, queue_size);
+      return Publisher(pub, topic, mem_size);
+    }
 
-      template < class M >
-      Subscriber< M > subscribe(const std::string & topic, uint32_t queue_size, void(*fp)(const boost::shared_ptr< const M > &)) {
-        SubscriberCallbackHelper< M > * phlp = new SubscriberCallbackHelper< M >(topic, fp);
-        ros::Subscriber sub = nh_->subscribe(topic, queue_size, &SubscriberCallbackHelper< M >::callback, phlp);
-        return Subscriber< M >(sub, phlp);
-      }
+    template < class M >
+    Subscriber< M > subscribe(const std::string & topic, uint32_t queue_size, void(*fp)(const boost::shared_ptr< const M > &)) {
+      SubscriberCallbackHelper< M > * phlp = new SubscriberCallbackHelper< M >(topic, fp);
+      ros::Subscriber sub = nh_->subscribe(topic, queue_size, &SubscriberCallbackHelper< M >::callback, phlp);
+      return Subscriber< M >(sub, phlp);
+    }
 
-    private:
-      boost::shared_ptr< ros::NodeHandle > nh_;
+  private:
+    boost::shared_ptr< ros::NodeHandle > nh_;
   };
 
 }
